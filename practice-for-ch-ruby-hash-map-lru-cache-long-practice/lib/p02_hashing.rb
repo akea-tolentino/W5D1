ccbb355735cc4b08159859ff311ab0bb43e47c9e
vincentpham123 <vincentpham32@gmail.com>
2023-04-24 15:24:37 -0700

    finished hashing

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice/lib/p02_hashing.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    result=0
+    self.each_with_index do |ele,index|
+      result+= ele.hash * index
+    end
+    result
   end
 end
 
 class String
   def hash
+    alphabet_range = ('a'..'z').to_a 
+    result=0
+    str_array = self.split('')
+    # str_array.sort! 
+    str_array.each.with_index do |char,index|
+      result += alphabet_range.index(char.downcase).hash * index
+    end
+    result
   end
 end
 
@@ -16,6 +29,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    array=[]
+    self.each do |key,value|
+      array << [key,value]
+    end
+    array.sort.flatten.hash
   end
 end
